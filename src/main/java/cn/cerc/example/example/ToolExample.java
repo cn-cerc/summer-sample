cda9761603ed94b85cd1c48912724b3949999b21
黄荣君 <l1091462907@gmail.com>
2019-03-27 23:53:06 +0800

    Update ToolExample.java

--- src/main/java/cn/cerc/example/example/ToolExample.java
+++ src/main/java/cn/cerc/example/example/ToolExample.java
@@ -13,6 +13,7 @@ public class ToolExample {
     public static void main(String[] args) {
         // 时间工具类 TDateTime
         log.info("当前时间 {}", TDateTime.Now());
+
         // 数学工具类 utils
         log.info("随机数字 {}", utils.getNumRandom(6));
         log.info("随机字符 {}", utils.getStrRandom(6));
