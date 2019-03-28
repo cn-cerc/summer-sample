105d2e6ed9e83978e99d276b5875a8a16bb79937
黄荣君 <l1091462907@gmail.com>
2019-03-28 09:43:42 +0800

    Update ToolExample.java

--- src/main/java/cn/cerc/example/example/ToolExample.java
+++ src/main/java/cn/cerc/example/example/ToolExample.java
@@ -1,5 +1,7 @@
 package cn.cerc.example.example;
 
+import cn.cerc.jdb.core.DataSet;
+import cn.cerc.jdb.core.Record;
 import cn.cerc.jdb.core.TDateTime;
 import cn.cerc.jdb.other.utils;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,25 @@ public class ToolExample {
         // 数学工具类 utils
         log.info("随机数字 {}", utils.getNumRandom(6));
         log.info("随机字符 {}", utils.getStrRandom(6));
+
+        DataSet dataSet = new DataSet();
+        Record head = dataSet.getHead();
+        head.setField("tbNo", "AB201903270001");
+        head.setField("tDate", TDateTime.Now());
+
+        for (int i = 0; i < 2; i++) {
+            dataSet.append();
+            dataSet.setField("code_", i);
+            dataSet.setField("name_", "name_" + i);
+        }
+        log.info("{}", head);
+        log.info("{}", dataSet);
+
+        while (dataSet.fetch()) {
+            String code = dataSet.getString("code_");
+            String name = dataSet.getString("name_");
+            log.info("{}-{}", code, name);
+        }
     }
 
 }
