2d297fce37466ca16811768ecbf7296ce9016d1e
IT君 <l1091462907@gmail.com>
2021-03-09 11:33:22 +0800

    Update StartPublic.java

--- src/main/java/cn/cerc/summer/sample/core/StartPublic.java
+++ src/main/java/cn/cerc/summer/sample/core/StartPublic.java
@@ -32,15 +32,15 @@ public class StartPublic implements ApplicationContextAware {
     public ModelAndView execute(@PathVariable String formId) {
         String funcId = "execute";
         String[] args = request.getRequestURI().split("/");
-        for(int i =0; i< args.length; i++) {
-            if(args[i].startsWith(formId)) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith(formId)) {
                 String[] tmp = args[i].split("\\.");
-                if(tmp.length > 1)
+                if (tmp.length > 1)
                     funcId = tmp[1];
                 break;
             }
         }
-             
+
         Application.setContext(context);
         String viewId = Application.getFormView(request, response, formId, funcId);
         return viewId != null ? new ModelAndView(viewId) : null;
