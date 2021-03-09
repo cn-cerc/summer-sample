08c9e709ec576e8af93749760ec8ad0392729f7b
张弓 <sz9214e@qq.com>
2021-03-09 17:49:31 +0800

    增加权限代码判断范例

--- src/main/java/cn/cerc/summer/sample/core/Passport.java
+++ src/main/java/cn/cerc/summer/sample/core/Passport.java
@@ -1,14 +1,21 @@
 package cn.cerc.summer.sample.core;
 
+import org.springframework.stereotype.Component;
+
 import cn.cerc.mis.core.AbstractHandle;
 import cn.cerc.mis.core.IPassport;
 import cn.cerc.mis.rds.PassportRecord;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Component
 public class Passport extends AbstractHandle implements IPassport {
 
     @Override
     public boolean passProc(String versions, String procCode) {
-        return true;
+        log.info("versions:{}, procCode: {}", versions, procCode);
+        //根据当前token，去数据库检查是否拥有指定的 procCode
+        return "user.base".equals(procCode);
     }
 
     @Override
