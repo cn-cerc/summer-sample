85dff69b6e36b94df78a52c6ab6561cdddfa13ff
张弓 <sz9214e@qq.com>
2021-03-13 14:23:09 +0800

    Delete SystemTable.java

--- src/main/java/cn/cerc/summer/sample/core/SystemTable.java
+++
@@ -1,9 +0,0 @@
-package cn.cerc.summer.sample.core;
-
-import cn.cerc.mis.config.SystemTableDefault;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SystemTable extends SystemTableDefault {
-
-}
