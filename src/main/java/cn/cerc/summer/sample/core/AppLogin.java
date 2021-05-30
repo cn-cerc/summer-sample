72decdbec73837389154b932140c8314b8b509f4
IT君 <l1091462907@gmail.com>
2021-05-30 20:17:36 +0800

    Update AppLogin.java

--- src/main/java/cn/cerc/summer/sample/core/AppLogin.java
+++ src/main/java/cn/cerc/summer/sample/core/AppLogin.java
@@ -65,9 +65,13 @@ public class AppLogin implements IAppLogin {
         return LOGIN_JSPFILE;
     }
 
-    @Override
     public String getLoginPage() throws IOException, ServletException {
         return LOGIN_JSPFILE;
+    }
+
+    @Override
+    public String checkToken(String token) throws IOException, ServletException {
+        return null;
     }
 
 }
