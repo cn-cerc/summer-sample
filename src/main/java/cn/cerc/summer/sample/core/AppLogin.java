5c079b047c357b15109c0d1cbf0dcd02094c9eb6
黄荣君 <l1091462907@gmail.com>
2021-06-11 08:38:14 +0800

    !4 Update AppLogin.java

--- src/main/java/cn/cerc/summer/sample/core/AppLogin.java
+++ src/main/java/cn/cerc/summer/sample/core/AppLogin.java
@@ -13,56 +13,29 @@ import org.springframework.stereotype.Component;
 import cn.cerc.core.ISession;
 import cn.cerc.mis.core.IAppLogin;
 import cn.cerc.mis.core.IForm;
-import cn.cerc.mis.core.RequestData;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class AppLogin implements IAppLogin {
     private static final Logger log = LoggerFactory.getLogger(AppLogin.class);
-    private static String LOGIN_JSPFILE = "login.jsp";
-    private static String TEMP_SESSIONID = "88888888";
-    private IForm form;
-    private ISession session;
 
     @Override
-    public void init(IForm form) {
-        this.form = form;
-        this.setSession(form.getSession());
-    }
-
-    @Override
-    public String checkLogin(String userCode, String password) throws IOException, ServletException {
-        log.info("userCode:{}, password:{}", userCode, password);
-
-        // 此处应去数据库检验
-        if ("admin".equals(userCode) && "123456".equals(password)) {
-            // 验证通过后，创建token并记录到Session中
-            form.getRequest().getSession().setAttribute(RequestData.TOKEN, TEMP_SESSIONID);
-            return null;
+    public String getLoginView(IForm form) throws IOException, ServletException {
+        String userCode = form.getRequest().getParameter("login_usr");
+        String password = form.getRequest().getParameter("login_pwd");
+        // 若页面有传递用户帐号，则强制重新校验
+        if (userCode != null && password != null) {
+            log.info("userCode:{}, password:{}", userCode, password);
+            // 此处应去数据库检验
+            if ("admin".equals(userCode) && "123456".equals(password)) {
+                // 验证通过后，创建token并记录到Session中
+                form.getRequest().getSession().setAttribute(ISession.TOKEN, "88888888");
+                return null;
+            }
         }
 
         // 显示登录页面的jsp文件名
-        return LOGIN_JSPFILE;
-    }
-
-    @Override
-    public ISession getSession() {
-        return session;
-    }
-
-    @Override
-    public void setSession(ISession session) {
-        this.session = session;
-    }
-
-    @Override
-    public String getJspFile() {
-        return LOGIN_JSPFILE;
-    }
-
-    @Override
-    public String getLoginPage() throws IOException, ServletException {
-        return LOGIN_JSPFILE;
+        return "login.jsp";
     }
 
 }
