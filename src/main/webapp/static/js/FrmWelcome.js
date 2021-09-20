6a021d0b1835161c811e064ec0c5b1f961989e4e
张弓 <sz9214e@qq.com>
2021-09-20 20:18:21 +0800

    修正无法重复添加的异常

--- src/main/webapp/static/js/FrmWelcome.js
+++ src/main/webapp/static/js/FrmWelcome.js
@@ -1,49 +1,62 @@
 import DataSet from "./db/DataSet.js";
-import UIPage from "./ui/UIPage.js";
 import UIGrid from "./ui/UIGrid.js";
 import UIButton from "./ui/UIButton.js";
 import UISpan from "./ui/UISpan.js";
+import UICustomPage from "./ui/UICustomPage.js";
 
-export let page = new UIPage();
 export let ds = new DataSet();
 
-let btnShow = new UIButton(page);
-btnShow.setId('btnShow');
-btnShow.setText("显示");
+export default class UIPage extends UICustomPage {
+    constructor(owner) {
+        super(owner);
+        let btnShow = new UIButton(this);
+        btnShow.setId('btnShow');
+        btnShow.setText("显示");
 
-new UISpan(page).setText(' ');
-let btnAppend = new UIButton(page);
-btnAppend.setId('btnAppend');
-btnAppend.setText("增加");
+        new UISpan(this).setText(' ');
+        let btnAppend = new UIButton(this);
+        btnAppend.setId('btnAppend');
+        btnAppend.setText("增加");
 
-new UISpan(page).setText(' ');
-let btnDelete = new UIButton(page);
-btnDelete.setId('btnDelete');
-btnDelete.setText("删除");
+        new UISpan(this).setText(' ');
+        let btnDelete = new UIButton(this);
+        btnDelete.setId('btnDelete');
+        btnDelete.setText("删除");
+    }
 
-page.setId('content');
-page.repaint();
-
-let grid = new UIGrid(page);
-grid.setDataSet(ds);
-
-document.getElementById(btnShow.getId())
-    .addEventListener('click', () => {
+    btnShow = () => {
         page.repaint();
-    });
+    };
 
-document.getElementById(btnAppend.getId())
-    .addEventListener('click', () => {
+    btnAppend = () => {
         ds.append();
         ds.setField('code', 'd');
         ds.setField('name', 'abcd');
         ds.setField('size', ds.size());
-        page.repaint();
-    });
+        this.repaint();
+    };
 
-document.getElementById(btnDelete.getId())
-    .addEventListener('click', () => {
+    btnDelete = () => {
         ds.delete();
-        page.repaint();
-    });
-    
+        this.repaint();
+
+    };
+
+    repaint = () => {
+        super.repaint();
+        document.getElementById('btnShow').addEventListener('click', this.btnShow);
+        document.getElementById('btnAppend').addEventListener('click', this.btnAppend);
+        document.getElementById('btnDelete').addEventListener('click', this.btnDelete);
+    }
+
+}
+
+export let page = new UIPage();
+page.setId('content');
+
+let grid = new UIGrid(page);
+grid.setDataSet(ds);
+
+page.repaint();
+
+
--- src/main/webapp/static/js/ui/UICustomPage.js
+++ src/main/webapp/static/js/ui/UICustomPage.js
@@ -1,7 +1,7 @@
 import UIComponent  from "./UIComponent.js";
 import HtmlWriter  from "./HtmlWriter.js";
 
-export default class UIPage extends UIComponent {
+export default class UICustomPage extends UIComponent {
 
     constructor(owner) {
         super(owner);
