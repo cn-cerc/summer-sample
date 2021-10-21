b57419aef4a6ab9d77a1727a14c8826e26bd8001
张弓 <sz9214e@qq.com>
2021-10-21 08:42:31 +0800

    Update LoginSystem.java

--- src/main/java/cn/cerc/summer/sample/services/LoginSystem.java
+++ src/main/java/cn/cerc/summer/sample/services/LoginSystem.java
@@ -3,16 +3,20 @@ package cn.cerc.summer.sample.services;
 import cn.cerc.core.DataSet;
 import cn.cerc.core.ISession;
 import cn.cerc.db.core.IHandle;
+import cn.cerc.mis.core.Permission;
 import cn.cerc.mis.core.ServiceException;
 import cn.cerc.mis.core.WebService;
 import cn.cerc.mis.security.SecurityService;
 
 @WebService(describe = "用户登录服务")
+@Permission(Permission.GUEST)
 public class LoginSystem implements SecurityService {
 
     @Override
     public DataSet execute(IHandle handle, DataSet dataIn) throws ServiceException {
-        return null;
+        DataSet out = new DataSet();
+        out.getHead().setValue("token", "1234567890");
+        return out.setState(1);
     }
 
     @Override
