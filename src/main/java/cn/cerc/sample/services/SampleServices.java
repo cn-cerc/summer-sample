1e51f33f9ccbb221afb11c1a625a215427826190
HUANG Rongjun <l1091462907@gmail.com>
2022-05-20 12:03:32 +0800

    清理不需要的服务声明

--- src/main/java/cn/cerc/sample/services/SampleServices.java
+++ src/main/java/cn/cerc/sample/services/SampleServices.java
@@ -3,52 +3,30 @@ package cn.cerc.sample.services;
 import java.util.Set;
 
 import cn.cerc.mis.client.ServiceSign;
+import cn.cerc.sample.services.proxy.UserCenterProxy;
 
 public class SampleServices {
-    public static class SvrUiExample {
+
+    /**
+     * 用户中心服务代理
+     */
+    private static final UserCenterProxy proxy = new UserCenterProxy();
+
+    public static class SvrEmployee {
         /** 新增人员信息 */
-        public static final ServiceSign append = new ServiceSign("SvrUiExample.append").setVersion(5)
-                .setProperties(Set.of("code_", "name_", "sex_", "age_"));
+        public static final ServiceSign append = new ServiceSign("SvrEmployee.append", proxy).setVersion(5)
+                .setProperties(Set.of("code_", "name_", "gender_", "age_"));
         /** 删除人员信息 */
-        public static final ServiceSign delete = new ServiceSign("SvrUiExample.delete").setVersion(5)
+        public static final ServiceSign delete = new ServiceSign("SvrEmployee.delete", proxy).setVersion(5)
                 .setProperties(Set.of("code_"));
         /** 获取人员信息 */
-        public static final ServiceSign download = new ServiceSign("SvrUiExample.download").setVersion(3)
+        public static final ServiceSign download = new ServiceSign("SvrEmployee.download", proxy).setVersion(3)
                 .setProperties(Set.of("code_"));
         /** 修改人员信息 */
-        public static final ServiceSign modify = new ServiceSign("SvrUiExample.modify").setVersion(5)
-                .setProperties(Set.of("code_", "sex_"));
+        public static final ServiceSign modify = new ServiceSign("SvrEmployee.modify", proxy).setVersion(5)
+                .setProperties(Set.of("code_", "gender_"));
         /** 根据条件查询人员信息 */
-        public static final ServiceSign search = new ServiceSign("SvrUiExample.search").setVersion(3);
-    }
-
-    public static class SvrExample {
-        /** 新增人员信息 */
-        public static final ServiceSign append = new ServiceSign("SvrExample.append").setVersion(5)
-                .setProperties(Set.of("code_", "name_", "sex_", "age_"));
-        /** 删除人员信息 */
-        public static final ServiceSign delete = new ServiceSign("SvrExample.delete").setVersion(5)
-                .setProperties(Set.of("code_"));
-        /** 获取人员信息 */
-        public static final ServiceSign download = new ServiceSign("SvrExample.download").setVersion(3)
-                .setProperties(Set.of("code_"));
-        /** 修改人员信息 */
-        public static final ServiceSign modify = new ServiceSign("SvrExample.modify").setVersion(5)
-                .setProperties(Set.of("code_", "sex_"));
-        /** 根据条件查询人员信息 */
-        public static final ServiceSign search = new ServiceSign("SvrExample.search").setVersion(3);
-    }
-
-    public static class SvrUiExampleMysql {
-        public static final ServiceSign append = new ServiceSign("SvrUiExampleMysql.append").setVersion(5)
-                .setProperties(Set.of("code_", "name_", "sex_"));
-        public static final ServiceSign delete = new ServiceSign("SvrUiExampleMysql.delete").setVersion(5)
-                .setProperties(Set.of("code_"));
-        public static final ServiceSign download = new ServiceSign("SvrUiExampleMysql.download").setVersion(3)
-                .setProperties(Set.of("code_"));
-        public static final ServiceSign modify = new ServiceSign("SvrUiExampleMysql.modify").setVersion(5)
-                .setProperties(Set.of("code_", "name_", "sex_"));
-        public static final ServiceSign search = new ServiceSign("SvrUiExampleMysql.search").setVersion(3);
+        public static final ServiceSign search = new ServiceSign("SvrEmployee.search", proxy).setVersion(3);
     }
 
 }
