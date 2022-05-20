7452b516a2e03d98c9e2ce82cae9392aee286945
HUANG Rongjun <l1091462907@gmail.com>
2022-05-20 12:02:55 +0800

    删除不需要的对象

--- src/main/java/cn/cerc/sample/forms/FrmBuildCode.java
+++ src/main/java/cn/cerc/sample/forms/FrmBuildCode.java
@@ -41,7 +41,7 @@ public class FrmBuildCode extends AbstractForm {
 
         UIInput input = new UIInput(form);
         input.setName("请输入表名: ");
-        input.setValue(reader.getString(input, "s_example"));
+        input.setValue(reader.getString(input, "s_employee"));
 
         UIButtonSubmit submit = new UIButtonSubmit(form);
         submit.setText("确认");
